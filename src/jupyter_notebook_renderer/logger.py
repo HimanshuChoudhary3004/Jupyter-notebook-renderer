6b27ae72ef1e70ffb88e7f46debc26429abc7c38
Himanshu Choudhary <Hchoudhary525@gmail.com>
2024-02-20 23:22:39 +0530

    updated logger file

--- src/jupyter_notebook_renderer/logger.py
+++ src/jupyter_notebook_renderer/logger.py
@@ -3,7 +3,7 @@ import logging
 import sys
 
 
-logging_str = "[%(asctime)s : %(level)s : %(module)s] : %(message)s"
+logging_str = "[%(asctime)s : %(levelname)s : %(module)s] : %(message)s"
 log_dir = "./logs"
 log_filepath = os.path.join(log_dir, "running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
